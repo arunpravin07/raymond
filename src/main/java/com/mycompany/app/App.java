a396574b8946d21a6d2eb82a439b579560691278
arunpravin07 <43900333+arunpravin07@users.noreply.github.com>
2021-05-24 18:10:37 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "thakida thakida " );
+		System.out.println( " " );
+		System.out.println( "oooo ooo oo o " );
+		System.out.println( " " );
+		System.out.println( "rakida rakida ----> o ooo ooo rakida rakida" );
+    }
+}
